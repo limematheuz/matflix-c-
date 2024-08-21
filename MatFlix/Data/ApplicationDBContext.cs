8f8459f815241cbd470e558e330f1cdbcda1a5db
limematheuz <mat.rib.lima@gmail.com>
2024-08-21 16:20:30 +0200

    Configured JWT in Program.cs, updated ApplicationDBContext for roles, created migrations, and added new controller for user validation.

---
+++ MatFlix/Data/ApplicationDBContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+using MatFlix.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatFlix.Data;
+
+public class ApplicationDBContext : IdentityDbContext<AppUser>
+{
+    public ApplicationDBContext(DbContextOptions bdContextOptions) : base(bdContextOptions)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        List<IdentityRole> roles = new List<IdentityRole>
+        {
+            new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "USER"
+            },
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
+    }
+}
