2574c0415d12d7241466629e2953748d1ccc3ab6
limematheuz <mat.rib.lima@gmail.com>
2024-08-23 12:15:15 +0200

    Add_Login_Endpoint_To_AccountController; add ConfigureSwaggerWithJwtBearerAuthentication to program

--- MatFlix/Services/TokenService.cs
+++ MatFlix/Services/TokenService.cs
@@ -31,7 +31,7 @@ public class TokenService : ITokenService
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.Now.AddDays(7),
+            Expires = DateTime.Now.AddHours(double.Parse(_config["JWT:ExpiryHours"])),
             SigningCredentials = creds,
             Issuer = _config["JWT:Issuer"],
             Audience = _config["JWT:Audience"]
