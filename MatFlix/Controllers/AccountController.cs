0c9b0898139fec79c618436e778a65276e4eccde
limematheuz <mat.rib.lima@gmail.com>
2024-08-23 12:00:56 +0200

    Add_Login_Endpoint_To_AccountController; add ConfigureSwaggerWithJwtBearerAuthentication to program

--- MatFlix/Controllers/AccountController.cs
+++ MatFlix/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using MatFlix.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MatFlix.Controllers
 {
@@ -13,11 +14,37 @@ namespace MatFlix.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService,
+            SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
+
+            if (user == null)
+                return Unauthorized("Invalid username or password!");
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            
+            if(!result.Succeeded)
+                return Unauthorized("Invalid username or password!");
+            return Ok(
+                new NewUserDto
+                {
+                    UserName = user.UserName,
+                    Email = user.Email, 
+                    Token = _tokenService.CreateToken(user)
+                });
         }
 
         [HttpPost("register")]
@@ -62,7 +89,5 @@ namespace MatFlix.Controllers
                 return StatusCode(500, e.Message);
             }
         }
-        
-        
     }
 }
---
+++ MatFlix/Dtos/Account/LoginDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MatFlix.Dtos;
+
+public class LoginDto
+{
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
